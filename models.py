175a6f4732be725a9f7ffdf2d2d341cf4d2eb5cc
0xa <xa@xomg.net>
2013-07-01 00:02:06 +0200

    Initial commit

---
+++ models.py
@@ -0,0 +1,134 @@
+from sqlalchemy import *
+from sqlalchemy.orm import relationship, backref
+import datetime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = MetaData()
+
+class Users(Base):
+    __tablename__ = 'users'
+    id       = Column(Integer, primary_key=True)
+    username = Column(String(32), unique=True, nullable=False)
+    password = Column(String(128))
+    email    = Column(String(512))
+    groupid  = Column(ForeignKey('groups.id'), nullable=False, default=0)
+    signup_date = Column(DateTime, default=datetime.datetime.now, nullable=False)
+    vhosts   = relationship('VHosts', backref='user')
+    logins   = relationship('LoginHistory', backref='user')
+    domains  = relationship('Domains', backref='user')
+    mailboxes= relationship('Mailboxes', backref='user')
+
+class Groups(Base):
+    __tablename__ = 'groups'
+    id       = Column(Integer, primary_key=True)
+    name     = Column(String(64), nullable=False)
+    description = Column(String(256))
+    perms    = Column(BigInteger, default=0, nullable=False)
+    appperms = Column(BigInteger, default=0, nullable=False)
+    users    = relationship('Users', backref='group')
+
+class LoginHistory(Base):
+    __tablename__ = 'login_history'
+    id       = Column(Integer, primary_key=True)
+    userid   = Column(ForeignKey('users.id'), nullable=False)
+    time     = Column(DateTime, default=datetime.datetime.now, nullable=False)
+    remote   = Column(String(64))
+    useragent= Column(String(64))
+
+class Servers(Base):
+    __tablename__ = 'servers'
+    id       = Column(Integer, primary_key=True)
+    name     = Column(String(32), unique=True, nullable=False)
+    fqdn     = Column(String(256), unique=True, nullable=False)
+    ipv4     = Column(String(32))
+    ipv6     = Column(String(64))
+    opened   = Column(Boolean, nullable=False)
+    lastupdate = Column(DateTime, nullable=False, default=datetime.datetime.fromtimestamp(1))
+    vhosts   = relationship('VHosts', backref='server')
+
+class Domains(Base):
+    __tablename__ = 'domains'
+    id       = Column(Integer, primary_key=True)
+    userid   = Column(ForeignKey('users.id'), nullable=False)
+    domain   = Column(String(256), nullable=False)
+    hostedns = Column(Boolean, nullable=False)
+    vhostid  = Column(ForeignKey('vhosts.id'))
+    public   = Column(Boolean, default=False, nullable=False)
+    verified = Column(Boolean, nullable=False, default=False)
+    verif_token = Column(String(64))
+    entries  = relationship('DomainEntries', backref='domain')
+    mailboxes= relationship('Mailboxes', backref='domain')
+
+class DomainEntries(Base):
+    __tablename__ = 'domainentries'
+    id       = Column(Integer, primary_key=True)
+    domainid = Column(ForeignKey('domains.id'), nullable=False)
+    sub      = Column(String(256))
+    rdatatype= Column(Integer, nullable=False)
+    rdata    = Column(Text, nullable=False)
+
+
+class Mailboxes(Base):
+    __tablename__ = 'mailboxes'
+    id       = Column(Integer, primary_key=True)
+    userid   = Column(ForeignKey('users.id'), nullable=False)
+    domainid = Column(ForeignKey('domains.id'), nullable=False)
+    local_part = Column(String(64), nullable=True)
+    password = Column(String(128))
+    redirect = Column(String(512))
+
+class VHosts(Base):
+    __tablename__ = 'vhosts'
+    
+    appTypes = {
+        0x00 : 'None',
+        0x01 : 'Custom HTTP',
+        0x02 : 'Custom FCGI',
+        0x04 : 'Custom WSGI',
+        0x10 : 'PHP',
+        0x20 : 'Python',
+        0x40 : 'Node.js',
+        0x80 : 'Perl',
+    }
+    
+    id       = Column(Integer, primary_key=True)
+    name     = Column(String(32), nullable=False)
+    userid   = Column(ForeignKey('users.id'), nullable=False)
+    serverid = Column(ForeignKey('servers.id'), nullable=False)
+    update   = Column(DateTime, onupdate=datetime.datetime.now)
+    catchall = Column(String(256))
+    autoindex= Column(Boolean, nullable=False, default=False)
+    apptype  = Column(BigInteger, nullable=False, default=0)
+    applocation = Column(String(512))
+    domains  = relationship('Domains', backref='vhost')
+    rewrites = relationship('VHostRewrites', backref='vhost')
+    acls     = relationship('VHostACLs', backref='vhost')
+    errorpages=relationship('VHostErrorPages', backref='vhost')
+
+class VHostRewrites(Base):
+    __tablename__ = 'vhostrewrites'
+    id       = Column(Integer, primary_key=True)
+    vhostid  = Column(ForeignKey('vhosts.id'), nullable=False)
+    regexp   = Column(String(256), nullable=False)
+    dest     = Column(String(256), nullable=False)
+    redirect_temp = Column(Boolean, nullable=False, default=False)
+    redirect_perm = Column(Boolean, nullable=False, default=False)
+    last     = Column(Boolean, nullable=False, default=False)
+
+class VHostACLs(Base):
+    __tablename__ = 'vhostacls'
+    id       = Column(Integer, primary_key=True)
+    title    = Column(String(256), nullable=False)
+    vhostid  = Column(ForeignKey('vhosts.id'), nullable=False)
+    regexp   = Column(String(256), nullable=False)
+    passwd   = Column(String(256), nullable=False)
+
+class VHostErrorPages(Base):
+    __tablename__ = 'vhosterrorpages'
+    id       = Column(Integer, primary_key=True)
+    vhostid  = Column(ForeignKey('vhosts.id'), nullable=False)
+    code     = Column(Integer, nullable=False)
+    path     = Column(String(256), nullable=False)
+
+
---
+++ tfhnode.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+"""
+Tux-FreeHost Node: Generate config for the current server.
+
+It tries to read a ./tfhnode.ini:
+[node]
+db = postgresql+psycopg2://tfhdev@localhost/tfhdev
+output-php = ./output/phpfpm/%s.conf
+output-nginx = ./output/nginx.conf
+"""
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from configparser import ConfigParser
+import logging
+import sys
+import datetime
+import os
+import socket
+from models import *
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from mako.template import Template
+from sqlalchemy import func
+
+# TODO:
+# - Add user if it does not exists
+# - Create home directory if it does not ex²ists
+# - require domain to be verified
+# - reload nginx
+# - DNS
+# - Postfix SQL queries on multiple lines.
+
+options = {
+    'db' : 'postgresql+psycopg2://tfhdev@localhost/tfhdev',
+    'output-php' : './output/phpfpm/%s.conf',
+    'output-nginx' : './output/nginx.conf',
+}
+
+parser = ArgumentParser(description=__doc__,
+    formatter_class=RawDescriptionHelpFormatter)
+parser.add_argument('--create-tables', action='store_true',
+    dest='create-tables', help='Create tables and exit.')
+parser.add_argument('--make-postfix', action='store_true',
+    dest='make-postfix', help='Generate postfix scripts and exit.')
+parser.add_argument('-v', '--verbose', action='count',
+    default=None, dest='verbose', help='Increase verbosity')
+parser.add_argument('--db', action='store', dest='db',
+    help='Set database info.')
+parser.add_argument('--output-php', action='store',
+    dest='output-php', help='Set PHP config file. %%s=user')
+parser.add_argument('--output-nginx', action='store',
+    dest='output-nginx', help='Set nginx config file.')
+parser.add_argument('--hostname', action='store',
+    dest='hostname', help='Set hostname. Use system\'s if omitted.')
+
+cli_options = vars(parser.parse_args())
+config = ConfigParser()
+config.read('./tfhnode.ini')
+if 'node' in config:
+    for d in config.items('node'):
+        options[d[0]] = d[1]
+else:
+    options = dict()
+for key in dict(cli_options).keys():
+    if cli_options[key] is not None:
+        options[key] = cli_options[key]
+
+log_level = logging.WARNING
+if options['verbose'] != None:
+    verbose = int(options['verbose'])
+    if verbose == 1:
+        log_level = logging.INFO
+    elif verbose >= 2:
+        log_level = logging.DEBUG
+logging.basicConfig(level=log_level)
+
+dbe = create_engine(options['db'])
+
+if options['create-tables']:
+    print('Creating tables...', end='')
+    Base.metadata.create_all(dbe)
+    print('DONE.')
+    exit(0)
+
+if options['make-postfix']:
+    print('Generating postfix scripts...')
+    header = 'hosts = %s\nuser = %s\npassword = %s\ndbname = %s\n'%(
+        dbe.url.host, dbe.url.user, dbe.url.password, dbe.url.database)
+    files = {
+        'domains' : "SELECT '%s' AS output FROM mailboxes LEFT JOIN domains ON domains.id = mailboxes.domainid WHERE domain='%s' LIMIT 1;",
+        'boxes' : "SELECT '%d/%u' FROM mailboxes LEFT JOIN domains ON domains.id = mailboxes.domainid WHERE local_part='%u' AND domain='%d' AND redirect IS NULL",
+        'aliases' : "SELECT redirect FROM mailboxes LEFT JOIN domains ON domains.id = mailboxes.domainid WHERE (local_part='%u' AND domain='%d' AND redirect IS NOT NULL) OR (local_part IS NULL AND domain='%d' AND redirect IS NOT NULL AND (SELECT COUNT(*) FROM mailboxes LEFT JOIN domains ON domains.id = mailboxes.domainid WHERE local_part='%u' AND domain='%d') = 0)",
+    }
+    for f in files.keys():
+        fh=open('./postfix/'+f+'.cf', 'w')
+        fh.write(header)
+        fh.write('query = '+files[f])
+        fh.close()
+    exit(0)
+
+dbs = sessionmaker(bind=dbe)()
+
+if 'hostname' in options:
+    hostname = options[hostname]
+else:
+    hostname = socket.gethostname()
+
+logging.info('Server: hostname is %s'%(hostname))
+
+server = dbs.query(Servers).filter_by(fqdn=hostname).first()
+if not server:
+    logging.critical('Cannot find server id.')
+    exit(1)
+logging.info('Server: #%d Last run: %s'%(server.id, server.lastupdate))
+
+tplNginx = Template(filename='./templates/nginx.conf')
+tplPhp = Template(filename='./templates/phpfpm.conf')
+tplBind = Template(filename='./templates/bind.conf')
+
+
+subq = dbs.query(func.count(VHosts.id).label('vhcount')) \
+.filter(and_(VHosts.userid==Users.id, VHosts.server==server)) \
+.subquery()
+users = dbs.query(Users).filter(subq.as_scalar()>=1).all()
+
+for user in users:
+    logging.info('Processing user #%d <%s>'%(user.id, user.username))
+    fh = open(options['output-php']%(user.username), 'w')
+    fh.write(tplPhp.render(
+        user = user.username,
+    ))
+    fh.close()
+
+fhNginx = open(options['output-nginx'], 'w')
+vhosts = dbs.query(VHosts).filter_by(server=server).all()
+for vhost in vhosts:
+    logging.info('Processing vhost #%d <%s>'%(vhost.id, vhost.name))
+    if len(vhost.domains) < 1:
+        logging.warning('vhost#%d: no domain.'%(vhost.id))
+        continue
+    for d in vhost.domains:
+        logging.debug('-> domain: %s'%d.domain)
+    fhNginx.write(tplNginx.render(
+        listen_addr = ('127.0.0.1', '::1'),
+        user = vhost.user.username,
+        name = vhost.name,
+        hostnames = ' '.join([d.domain for d in vhost.domains]),
+        autoindex = vhost.autoindex,
+        catchall = vhost.catchall,
+        rewrites = vhost.rewrites,
+        error_pages = vhost.errorpages,
+        acl = vhost.acls
+    ))
+fhNginx.close()
+
+
+
+server.lastupdate = datetime.datetime.now()
+dbs.commit()
+
