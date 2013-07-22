b605be5b4866d389659cdb91cebe40547fdfaa78
0xa <xa@xomg.net>
2013-07-22 04:34:33 +0200

    Add a script to make symlinks.

---
+++ symlinks.sh
@@ -0,0 +1,5 @@
+ln -s /root/tfhnode/output/pam_pgsql.conf /etc/pam_pgsql.conf
+ln -s /root/tfhnode/output/nss-pgsql.conf /etc/nss-pgsql.conf
+ln -s /root/tfhnode/output/nss-pgsql-root.conf /etc/nss-pgsql-root.conf
+
+
