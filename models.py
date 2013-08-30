c76be874aa1063d12169cffe9c8b408ad8098025
0xa <xa@xomg.net>
2013-08-30 18:43:17 +0200

    Add Service models

--- models.py
+++ models.py
@@ -2,6 +2,11 @@ from sqlalchemy import *
 from sqlalchemy.orm import relationship, backref
 import datetime
 from sqlalchemy.ext.declarative import declarative_base
+import logging
+import sys
+import os
+import socket
+from mako.template import Template
 
 Base = declarative_base()
 metadata = MetaData()
@@ -131,5 +136,148 @@ class VHostErrorPages(Base):
     vhostid  = Column(ForeignKey('vhosts.id'), nullable=False)
     code     = Column(Integer, nullable=False)
     path     = Column(String(256), nullable=False)
+
+
+class Service(object):
+    def clear(self):
+        if hasattr(self, 'output_dir'):
+            for f in os.listdir(self.output_dir):
+                if f.endswith(self.output_ext):
+                    os.remove(self.output_dir+'/'+f)
+        elif hasattr(self, 'output_file'):
+            if os.path.isfile(self.output_file):
+                os.remove(self.output_file)
+            
+    def reload(self):
+        if hasattr(self, 'pidfile'):
+            if hasattr(self, 'reload_signal'):
+                signal = self.reload_signal
+            else:
+                signal = 'SIGHUP'
+        try:
+            pid = int(open(pidfile).read())
+            r = subprocess.call(['kill', '-'+signal, str(pid)])
+            if r != 0:
+                logging.error('Failed to sent %s to %s!', signal, process)
+        except FileNotFoundError:
+            logging.warning('Pidfile not found for '+process)
+    
+    def generate_vhost(self, vhost):
+        raise NotImplementedError()
+        
+
+class NginxService(Service):
+    def __init__(self, output, pidfile):
+        self.output_dir = output
+        self.output_ext = '.conf'
+        self.pidfile = pidfile
+        self.template = Template(filename='./templates/nginx.conf')
+
+    def generate_vhost(self, vhost):
+        filename = self.output_dir + '%s_%s.conf'%(
+            vhost.user.username, vhost.name)
+        fh = open(filename, 'w')
+        if len(vhost.domains) < 1:
+            logging.warning('vhost#%d/nginx: no domain associated.'%(vhost.id))
+            return
+            
+        if not vhost.path:
+            # Before vhost.path...
+            # TODO: Remove backward compatibility stuff and upgrade server
+            # TODO: vhost.path is not used. Is it useless?
+            pubdir = '/home/%s/http_%s/' % (vhost.user.username, vhost.name)
+            legacydir = '/home/%s/public_http/' % (vhost.user.username)
+            if not os.path.isdir(pubdir):
+                if os.path.isdir(legacydir):
+                    pubdir = legacydir
+                else:
+                    os.makedirs(pubdir)
+                    os.system('chown %s:%s -R %s'%(
+                        user.username, user.username, pubdir))
+            else:
+                pubdir = os.path.abspath(vhost.path)
+                if not pubdir.startswith('/home/'+vhost.user.username+'/'):
+                    # Should not be out of user's /home
+                    logging.warning('vhost#%d/nginx: invalid path.')
+                    return
+                if not os.path.isdir(pubdir):
+                    logging.warning('vhost#%d/nginx: path does not exists.')
+                    return
+                if getpwuid(stat(pubdir).st_uid).pw_name != vhost.user.username:
+                    logging.warning('vhost#%d/nginx: path does not belong to user.')
+                    return
+        
+        appsocket = None
+        if vhost.apptype == 0x20: # uwsgi
+            # TODO: Do that in templates, as for PHP
+            appsocket = '/var/lib/uwsgi/app_%s_%s.sock'%(
+                vhost.user.username, vhost.name)
+
+        for d in vhost.domains:
+            logging.debug('-> domain: %s'%d.domain)
+        
+        addresses = ['127.0.0.1', '::1']
+        if server.ipv4:
+            addresses.append(server.ipv4)
+        if server.ipv6:
+            addresses.append(server.ipv6)
+        
+        fh.write(self.template.render(
+            listen_addr = addresses,
+            user = vhost.user.username,
+            name = vhost.name,
+            pubdir = pubdir,
+            hostnames = ' '.join([d.domain for d in vhost.domains]),
+            autoindex = vhost.autoindex,
+            catchall = vhost.catchall,
+            rewrites = vhost.rewrites,
+            error_pages = vhost.errorpages,
+            acl = vhost.acls,
+            apptype = vhost.apptype,
+            appsocket = appsocket,
+            applocation = vhost.applocation,
+        ))
+        fh.close()
+    
+class UwsgiService(Service):
+    def __init__(self, output):
+        self.output_dir = output
+        self.output_ext = '.ini'
+        self.template = Template(filename='./templates/uwsgi.ini')
+
+    def generate_vhost(self, vhost):
+        # FIXME: Make check on vhost.applocation
+        filename = self.output_dir + '/%s_%s.ini'%(
+            vhost.user.username, vhost.name)
+        fh = open(filename, 'w')
+        fh.write(self.template.render(vhost=vhost, user=vhost.user))
+        fh.close()
+
+    def remove_vhost(self, vhost):
+        filename = self.output_dir + '/%s_%s.ini'%(
+            vhost.user.username, vhost.name)
+        if os.path.isfile(filename):
+            os.remove(filename)
+        
+class PhpfpmService(Service):
+    def __init__(self, output, pidfile):
+        self.output_dir = output
+        self.output_ext = '.conf'
+        self.pidfile = pidfile
+        self.template = Template(filename='./templates/phpfpm.conf')
+
+    def generate_vhost(self, vhost):
+        filename = self.output_dir + '%s.conf'%(vhost.user.username)
+        # Never need to be changed, only created/deleted
+        if not os.path.isfile(filename):
+            return
+        fh = open(filename, 'w')
+        fh.write(tplPhp.render(user=user.username))
+        fh.close()
+
+    def remove_vhost(self, vhost):
+        filename = self.output_dir + '%s.conf'%(vhost.user.username)
+        if os.path.isfile(filename):
+            os.remove(filename)
 
 
